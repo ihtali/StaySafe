3e98c9185b70c2785ac5c497c87ab767c6f7227c
ihtali <ihtashamia786@icloud.com>
2025-02-20 15:48:10 +0000

    Initial Commit

---
+++ StaySafe/StaySafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StaySafeApp.swift
+//  StaySafe
+//
+//  Created by Ihtasham Ali on 20/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct StaySafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
