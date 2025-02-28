1c97840538f103f5d8ec8e2f47a30db0b89d4842
ihtali <ihtashamia786@icloud.com>
2025-02-28 14:34:24 +0000

    Login,Registration,Home Page Structure Done

---
+++ StaySafe/Views/HomeView.swift
@@ -0,0 +1,23 @@
+//
+//  HomeView.swift
+//  StaySafe
+//
+//  Created by Ihtasham Ali on 28/02/2025.
+//
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Text("Welcome to Home Page!")
+                .font(.largeTitle)
+                .bold()
+                .padding()
+        }
+    }
+}
+
+
+#Preview {
+    HomeView()
+}
---
+++ StaySafe/Views/RegistrationView.swift
@@ -0,0 +1,55 @@
+//
+//  RegistrationView.swift
+//  StaySafe
+//
+//  Created by Ihtasham Ali on 28/02/2025.
+//
+import SwiftUI
+
+struct RegistrationView: View {
+    @State private var username = ""
+    @State private var email = ""
+    @State private var password = ""
+    @State private var isRegistered = false
+
+    var body: some View {
+        if isRegistered {
+            HomeView()
+        } else {
+            VStack {
+                Text("Register")
+                    .font(.largeTitle)
+                    .bold()
+
+                TextField("Username", text: $username)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+
+                TextField("Email", text: $email)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+
+                SecureField("Password", text: $password)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+
+                Button(action: {
+                    isRegistered = true // Navigate to Home Page
+                }) {
+                    Text("Sign Up")
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.green)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                .padding()
+            }
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    RegistrationView()
+}
