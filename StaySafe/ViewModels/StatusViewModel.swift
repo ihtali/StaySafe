380e2e4c23db6e27358c13b0d59cc2039ec9c09b
HEET PATEL <heetpatel152@icloud.com>
2025-03-07 16:30:04 +0000

    status ready

---
+++ StaySafe/ViewModels/StatusViewModel.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+class StatusViewModel: ObservableObject {
+    @Published var statuses: [Status] = []
+
+    func fetchStatuses() {
+        guard let url = URL(string: "https://softwarehub.uk/unibase/staysafe/v1/api/status") else {
+            print("Invalid URL")
+            return
+        }
+
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print("Request error:", error.localizedDescription)
+                return
+            }
+
+            guard let data = data else {
+                print("No data received")
+                return
+            }
+
+            do {
+                let decodedStatuses = try JSONDecoder().decode([Status].self, from: data)
+                DispatchQueue.main.async {
+                    self.statuses = decodedStatuses
+                }
+            } catch {
+                print("Decoding error:", error.localizedDescription)
+            }
+        }.resume()
+    }
+}
